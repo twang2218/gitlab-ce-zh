ac2cb363f66f25c0017d50381e798d8edfbb915f
Tao Wang <twang2218@gmail.com>
2017-01-12 18:35:03 +1100

    更新版本：8.15.4, 8.14.6, 8.13.11
    * 8.15.3 => 8.15.4
    * 8.14.5 => 8.14.6
    * 8.13.10 => 8.13.11
    
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- build-version.sh
+++ build-version.sh
@@ -3,16 +3,16 @@
 function generate() {
     version_8_11=8.11.11
     version_8_12=8.12.13
-    version_8_13=8.13.10
-    version_8_14=8.14.5
-    version_8_15=8.15.3
+    version_8_13=8.13.11
+    version_8_14=8.14.6
+    version_8_15=8.15.4
 
     generate_branch_dockerfile  ${version_8_11}-ce.0    v${version_8_11}    8-11-stable-zh          > 8.11/Dockerfile
     generate_tag_dockerfile     ${version_8_12}-ce.0    v${version_8_12}    v${version_8_12}-zh     > 8.12/Dockerfile
     generate_tag_dockerfile     ${version_8_13}-ce.0    v${version_8_13}    v${version_8_13}-zh     > 8.13/Dockerfile
     generate_tag_dockerfile     ${version_8_14}-ce.0    v${version_8_14}    v${version_8_14}-zh     > 8.14/Dockerfile
-    generate_tag_dockerfile     ${version_8_15}-ce.0    v${version_8_15}    v${version_8_15}-zh     > 8.15/Dockerfile
-    generate_branch_dockerfile  ${version_8_15}-ce.0    v${version_8_15}    8-15-stable-zh          > testing/Dockerfile
+    generate_tag_dockerfile     ${version_8_15}-ce.1    v${version_8_15}    v${version_8_15}-zh     > 8.15/Dockerfile
+    generate_branch_dockerfile  ${version_8_15}-ce.1    v${version_8_15}    8-15-stable-zh          > testing/Dockerfile
 
     generate_docker_compose_yml ${version_8_15} > docker-compose.yml
 
