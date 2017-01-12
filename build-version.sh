dffcea2c84064ff9b9f3216ca7db063bd44fdf5c
Tao Wang <twang2218@gmail.com>
2017-01-12 17:57:11 +1100

    进一步将重复部分提出，以方便维护
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- build-version.sh
+++ build-version.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 function generate() {
-    generate_branch_dockerfile  8.11.11-ce.0    v8.11.11    8-11-stable-zh  > 8.11/Dockerfile
-    generate_tag_dockerfile     8.12.13-ce.0    v8.12.13    v8.12.13-zh     > 8.12/Dockerfile
-    generate_tag_dockerfile     8.13.10-ce.0    v8.13.10    v8.13.10-zh     > 8.13/Dockerfile
-    generate_tag_dockerfile     8.14.5-ce.0     v8.14.5     v8.14.5-zh      > 8.14/Dockerfile
-    generate_tag_dockerfile     8.15.3-ce.0     v8.15.3     v8.15.3-zh      > 8.15/Dockerfile
-    generate_branch_dockerfile  8.15.3-ce.0     v8.15.3     8-15-stable-zh  > testing/Dockerfile
+    version_8_11=8.11.11
+    version_8_12=8.12.13
+    version_8_13=8.13.10
+    version_8_14=8.14.5
+    version_8_15=8.15.3
 
-    generate_docker_compose_yml 8.15.3 > docker-compose.yml
+    generate_branch_dockerfile  ${version_8_11}-ce.0    v${version_8_11}    8-11-stable-zh          > 8.11/Dockerfile
+    generate_tag_dockerfile     ${version_8_12}-ce.0    v${version_8_12}    v${version_8_12}-zh     > 8.12/Dockerfile
+    generate_tag_dockerfile     ${version_8_13}-ce.0    v${version_8_13}    v${version_8_13}-zh     > 8.13/Dockerfile
+    generate_tag_dockerfile     ${version_8_14}-ce.0    v${version_8_14}    v${version_8_14}-zh     > 8.14/Dockerfile
+    generate_tag_dockerfile     ${version_8_15}-ce.0    v${version_8_15}    v${version_8_15}-zh     > 8.15/Dockerfile
+    generate_branch_dockerfile  ${version_8_15}-ce.0    v${version_8_15}    8-15-stable-zh          > testing/Dockerfile
+
+    generate_docker_compose_yml ${version_8_15} > docker-compose.yml
 
     generate_readme \
-        8.11.11 \
-        8.12.13 \
-        8.13.10 \
-        8.14.5 \
-        8.15.3 \
+        ${version_8_11} \
+        ${version_8_12} \
+        ${version_8_13} \
+        ${version_8_14} \
+        ${version_8_15} \
         > README.md
 }
