076bab37261c4702b8fb05adf88545142c6712a8
Tao Wang <twang2218@gmail.com>
2017-06-13 18:48:40 +1000

    升级到 9.2.2
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- build-version.sh
+++ build-version.sh
@@ -5,7 +5,7 @@ function generate() {
     version_8_17=8.17.6
     version_9_0=9.0.7
     version_9_1=9.1.4
-    version_9_2=9.2.1
+    version_9_2=9.2.2
     version_latest=${version_9_2}
     testing_version=${version_9_2}
     testing_tag=${testing_version}-ce.0
