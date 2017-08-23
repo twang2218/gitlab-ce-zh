a3356aef0ee9615149912b2866eaec21b37e71c1
Tao Wang <twang2218@gmail.com>
2017-08-23 20:46:54 +1000

    添加 9.5.0，移除 9.0 分支
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- versions.sh
+++ versions.sh
@@ -4,12 +4,11 @@
 # Unless there is an extra version branch for testing, such as, '-rc'
 #  Zero-based array
 export VERSIONS=(
-    9.0.13
     9.1.10
     9.2.10
     9.3.10
     9.4.5
-    9.5.0-rc5
+    9.5.0
 )
 
 export APPENDIX=(
@@ -18,11 +17,9 @@ export APPENDIX=(
     '-ce.0'
     '-ce.0'
     '-ce.0'
-    '.ce.0'
 )
 
 export GENERATORS=(
-    generate_tag_v8_17_dockerfile
     generate_tag_v8_17_dockerfile
     generate_tag_v8_17_dockerfile
     generate_tag_v8_17_dockerfile
@@ -34,6 +31,6 @@ export BRANCHES=($(for v in ${VERSIONS[@]}; do echo -n "${v%.*} "; done))
 
 # should be as simple as $VERSION[-1], however, bash on Mac is old
 # export VERSION_LATEST=${VERSIONS[-1]}
-export VERSION_LATEST=${VERSIONS[${#VERSIONS[@]}-2]}
+export VERSION_LATEST=${VERSIONS[${#VERSIONS[@]}-1]}
 export BRANCHES_LATEST=${VERSION_LATEST%.*}
 export APPENDIX_LATEST=-ce.0
